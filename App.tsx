d0bf7d586211f6276dc4c1758da00005e61b744d
Breno Guilherme de Souza Lopes <brenogdev@gmail.com>
2023-01-28 00:16:07 -0300

    (feat): adicionado axios e os assets

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
 import { Home } from "./src/screens/home";
@@ -6,6 +7,7 @@ import { Home } from "./src/screens/home";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor="transparent" translucent />
       <Home />
     </ThemeProvider>
   );
